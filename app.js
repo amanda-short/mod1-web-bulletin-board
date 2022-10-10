52aca56f828e0d1fd701cb9e46214e9b2f66bb1e
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-10-10 15:34:18 -0700

    post form added

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
+import { getPosts } from  '/fetch-utils';
 
 /* Get DOM Elements */
+const postList = document.getElementById('post-list');
 
 /* State */
+let error = null;
+let posts = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    const response = await getPosts();
+    error = response.error;
+    posts = response.data;
+
+    if (error) {
+        displayError();
+    }
+
+    if (posts) {
+        displayPosts();
+    }
+});
 
 /* Display Functions */
